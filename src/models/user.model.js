370bf3cb83d6b55cd7482cd2d62d6195603ba31a
Raman <ramantiwary19@gmail.com>
2025-09-26 15:37:39 +0530

    implement jwt and bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose,{Schema} from  "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
 
 const userSchema = new Schema(
     {
@@ -49,4 +52,49 @@ const userSchema = new Schema(
     }
 )
 
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+// check password is correct or not 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.password(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+   return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+
+
+}
+
 export const User = mongoose.model("User",userSchema)
+
+
+// userSchema.pre is hook it take next call back 
+// it is use to check the password is modified or not
+// we chech neagive if password is modified than it works
