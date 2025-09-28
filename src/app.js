ebb861720141b10d626a45807634a9c99a0fe986
Raman <ramantiwary19@gmail.com>
2025-09-28 15:31:46 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -10,7 +10,15 @@ app.use(cors({
     credentials:true
 }))
 
-app.use(express.json({limit}))
+app.use(express.json({limit:"16kb"}))
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration 
+app.use("/api/v1/users",userRouter)
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/db.js";   // also make sure you add `.js` here
 import dotenv from "dotenv";
+import {app} from './app.js'
 
 dotenv.config({
     path: "./env"
@@ -13,7 +14,7 @@ connectDB()
         console.log(`Server is running at port:${process.env.PORT}`);
     })
 })
-.catch(()=>{
+.catch((err)=>{
     console.log("Mongodb connection failed !!!",err)
 })
 
